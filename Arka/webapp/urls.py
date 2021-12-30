d29c12865415319f0de20494cc8ca97b98aeb999
its-man-ish <manishbsn188@gmail.com>
2021-12-30 22:31:08 +0530

    fixed navbar

--- Arka/webapp/urls.py
+++ Arka/webapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
    path('',IndexView.as_view(),name='home'),
    path('blogs',BlogsList.as_view(), name='blogs'),
    path('members',MembersList.as_view(), name='team'),
-   path('blog/<int:pk>',BlogDetail.as_view(), name='blog'),
+   path('blog/<int:pk>/',BlogDetail.as_view(), name='blog'),
   
    path('gallery/',GalleryList.as_view(), name = 'gallery'),
    path('admin-dashboard/',dashboard)
