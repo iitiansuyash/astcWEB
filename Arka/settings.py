baa70c6af17b2a3b2ece9a7c8a409c78039d80f1
its-man-ish <67169163+its-man-ish@users.noreply.github.com>
2021-12-31 23:16:53 +0530

    setting up allowed host

--- Arka/settings.py
+++ Arka/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2!gjhv_(xfqrh^_6&tgr=9zw2w78=--u9-xrjv@h_3b=_@szt1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['arkaclub.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
