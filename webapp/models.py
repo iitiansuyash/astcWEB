d226f3a1a1b93a8f3027c8f852137c7fd3d32f56
its-man-ish <manishbsn188@gmail.com>
2022-01-01 10:01:59 +0530

    team member page

--- webapp/models.py
+++ webapp/models.py
@@ -10,8 +10,8 @@ DEPARTMENT = (
     ('web-dev','Web Developer'),
     ('graphic','Graphic Designer'),
     ('content','Content-writer'),
-
     )
+
 class PostModel(models.Model):
      post = models.CharField(max_length=20, choices = POST_TYPE)
      title= models.CharField(max_length=230)
@@ -23,13 +23,26 @@ class PostModel(models.Model):
      def __str__(self):
         return self.title
 
+STATUS = (
+    ('Coordinator','Coordinator'),
+    ('Member','Member'),
+    ('Founder','Founder'),
+    ('Alumnus','Alumnus')
+) 
 
 class TeamMember(models.Model):
     name = models.CharField(max_length=200)
-    admission_number = models.CharField(max_length=200)
+    status =models.CharField(max_length=100, choices = STATUS)
     Arka_department = models.CharField(max_length=200)
-    profile_pic = models.ImageField(upload_to="./members", null=True, blank=True)
-    graduated = models.BooleanField(default=False)
+    profile_pic = models.ImageField(upload_to="./members")
+
+    admission_number = models.CharField(max_length=200,blank=True)
+    admission_year = models.CharField(max_length=200)
+    fb_url =models.URLField(max_length=300, blank=True, null=True)
+    linked_url =models.URLField(max_length=300 , blank=True, null=True)
+    insta_url =models.URLField(max_length=300 , blank=True, null=True)
+    twitter_url =models.URLField(max_length=300 , blank=True, null=True)
+
 
 class VideoModel(models.Model):
     title=models.CharField( max_length=200)
