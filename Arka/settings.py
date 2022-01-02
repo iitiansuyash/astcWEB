c53f2c96148c75fe515f9d78f3efd07409235034
its-man-ish <manishbsn188@gmail.com>
2022-01-02 17:08:10 +0000

    update

--- Arka/settings.py
+++ Arka/settings.py
@@ -25,7 +25,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'django-insecure-2!gjhv_(xfqrh^_6&tgr=9zw2w78=--u9-xrjv@h_3b=_@szt1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['arkaclub.pythonanywhere.com', '127.0.0.1', 'localhost']
 
